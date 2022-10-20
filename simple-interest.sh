f9670a0b30768e853ea21f7e3805bba42a1e33b6
abcd123456742 <115230860+abcd123456742@users.noreply.github.com>
2022-10-20 21:00:58 +0530

    Update simple-interest.sh
    author lab course entry

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,7 +2,7 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: abcd123456742
 # Addtional Authors:
 # <your Github username>
 
